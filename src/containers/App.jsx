b6df46d7e3d76a06e2a19defc01a3727563bcb62
Anibal Ledesma <afledesmacastro@gmail.com>
2020-05-29 12:31:08 -0500

    resolved error

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,17 +12,18 @@ const App = () => (
     <div className='App'>
         <div className='container'>
             <Header />
+            <hr/>
             <MainInfo
-                Name="Anibal"
-                LastName="Ledesma"
-                Occupation="Front End"
+                Name='Anibal'
+                LastName='Ledesma'
+                Occupation='Front End'
                 FaceUrl='https://www.facebook.com/aledesmadev/'
                 InUrl='https://www.linkedin.com/in/afledesma/'
                 Photo='https://scontent.fbog2-3.fna.fbcdn.net/v/t1.0-9/33516574_445271872580085_3127037534983421952_n.jpg?_nc_cat=109&_nc_sid=85a577&_nc_eui2=AeGRZY0cNwDsXgtB7lSo_FqcolcJCI2ICFmiVwkIjYgIWW4KOekpR5_x_qbSeewZric&_nc_ohc=BPJsrpbE-aMAX8zeU7e&_nc_ht=scontent.fbog2-3.fna&oh=72f878f9bac672e042050a546910274d&oe=5EF5932B'
             />
         </div>
-        <div className="container odd">
-            <GetInTouch phone="1234567890" email="aaaa@eee.com" />
+        <div className='container odd'>
+            <GetInTouch phone='1234567890' email='aaaa@eee.com' />
         </div>
     </div>
 );
